6b96e89652f89d5a936d07ba77783854fc30da8d
Thomas JOUANNOT <mazerty@gmail.com>
2019-07-21 06:01:31 +0200

    added spring initializr files

---
+++ src/main/java/org/lyonjug/parkoursup/ParkoursupApplication.java
@@ -0,0 +1,13 @@
+package org.lyonjug.parkoursup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkoursupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkoursupApplication.class, args);
+	}
+
+}
