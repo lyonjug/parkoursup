c6b2e2d74a39c3f109135c44e89d961c212381d5
Thomas JOUANNOT <mazerty@gmail.com>
2019-07-21 14:24:12 +0200

    now with a dialog

--- src/main/java/org/lyonjug/parkoursup/MainView.java
+++ src/main/java/org/lyonjug/parkoursup/MainView.java
@@ -1,6 +1,7 @@
 package org.lyonjug.parkoursup;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.function.ValueProvider;
@@ -12,8 +13,6 @@ import java.util.Random;
 
 @Route
 public class MainView extends VerticalLayout {
-
-	private final Grid<String> grid = new Grid<>();
 
 	private final List<String> list = Arrays.asList(
 			"@Jojo6375", "@Arcantos512", "@thofon", "@petitlaurent", "@indyHiTech",
@@ -28,11 +27,19 @@ public class MainView extends VerticalLayout {
 			"@cedric__chateau", "@k33g_org");
 
 	public MainView() {
+		setHeightFull();
+
 		var button = new Button("Tirer au sort");
-		button.addClickListener(event -> grid.select(list.get(new Random().nextInt(list.size()))));
+		button.addClickListener(event -> {
+			var dialog = new Dialog();
+			dialog.add(list.get(new Random().nextInt(list.size())));
+			dialog.open();
+		});
 		add(button);
 
+		var grid = new Grid<String>();
 		grid.setItems(list);
+		grid.setHeightFull();
 		grid.addColumn(ValueProvider.identity());
 		add(grid);
 	}
