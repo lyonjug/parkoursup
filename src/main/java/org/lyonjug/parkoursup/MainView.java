c386ddef247f43a9dd79eedb00e119d17580d6d1
Thomas JOUANNOT <mazerty@gmail.com>
2020-04-04 07:33:22 +0200

    lottery april 2020

--- src/main/java/org/lyonjug/parkoursup/MainView.java
+++ src/main/java/org/lyonjug/parkoursup/MainView.java
@@ -14,34 +14,32 @@ import java.util.Random;
 @Route
 public class MainView extends VerticalLayout {
 
-	private final List<String> list = Arrays.asList(
-			"@Jojo6375", "@Arcantos512", "@thofon", "@petitlaurent", "@indyHiTech",
-			"@jibe77", "@warn_Off", "@remi_mrn", "@noredine69", "@skarboune",
-			"@gVibrac", "@NoemiePham", "@Rodac84", "@FrancisCharpin", "@blskdan",
-			"@ccheneson", "@Bilgetz1", "@sadraskol", "@thebignet", "@DavidCharlet",
-			"@TimAufort", "@fabgrenier", "@toufik0117", "@guyon_jerome", "@romain_dequidt",
-			"@eDenayrolles", "@nafresne", "@AnnKalamity", "@dogiaf", "@joanna_lacante",
-			"@renarnaud", "@AurelienMino", "@adrienpessu", "@grenneville", "@TheBrousse",
-			"@mackristof", "@Puppet_2501", "@sanlaville", "@mekquispass", "@maxime_vialette",
-			"@GalletVictor", "@RiverJach", "@TFranconville", "@BonnefondVero", "@titimoby",
-			"@cedric__chateau", "@k33g_org");
+    private final List<String> list = Arrays.asList(
+            "@warn_Off", "@magneval", "@clpetot", "@t_marmillot", "@Kheon669",
+            "@AymaneHafiane", "@glgaws", "@OlivierPerez", "@jokeur76", "@esfe17",
+            "@thierryparet", "@thebignet", "@imNollo", "@maxime_vialette", "@guilldavid",
+            "@luc_chevallier", "@BucheNicolas", "@plugandtweet", "@MriniYacine", "@SamadouSare",
+            "@tttataww", "@coralie_noy", "@cedric__chateau", "@fnicollet", "@GuiF007",
+            "@foxoyote", "@noredine69", "@blskdan", "@Bilgetz1", "@GalletVictor",
+            "@fred532", "@maillotm", "@Jojo6375", "@LittleFr0g", "@arisoft31",
+            "@petitlaurent");
 
-	public MainView() {
-		setHeightFull();
+    public MainView() {
+        setHeightFull();
 
-		var button = new Button("Tirer au sort");
-		button.addClickListener(event -> {
-			var dialog = new Dialog();
-			dialog.add(list.get(new Random().nextInt(list.size())));
-			dialog.open();
-		});
-		add(button);
+        var button = new Button("Tirer au sort");
+        button.addClickListener(event -> {
+            var dialog = new Dialog();
+            dialog.add(list.get(new Random().nextInt(list.size())));
+            dialog.open();
+        });
+        add(button);
 
-		var grid = new Grid<String>();
-		grid.setItems(list);
-		grid.setHeightFull();
-		grid.addColumn(ValueProvider.identity());
-		add(grid);
-	}
+        var grid = new Grid<String>();
+        grid.setItems(list);
+        grid.setHeightFull();
+        grid.addColumn(ValueProvider.identity());
+        add(grid);
+    }
 
 }
